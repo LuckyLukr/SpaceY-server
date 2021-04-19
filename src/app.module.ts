9a21102a5da2bcdf5d4dae3166f8fb765640c2aa
Luke <mrlukr11@gmail.com>
2021-04-19 10:41:35 +0200

    atlas URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     UsersModule,
-    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MongooseModule.forRoot(process.env.ATLAS_URI),
     AuthModule
   ],
   controllers: [AppController],
